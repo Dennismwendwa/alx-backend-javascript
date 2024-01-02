43d5b3188dbd9a860a0dbfb93a611ecd8f99d837
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-02 23:41:26 +0300

    created objects

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  const reportObject = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+  };
+
+  return reportObject;
+}
