b6de6c5906c6283af1b694b06166fbd38f9b4e74
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-03 20:10:13 +0300

    multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
 import signUpUser from './4-user-promise';
 import upLoadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = upLoadPhoto(fileName);
 
   return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => {
-      return results.map((result) => ({
+    .then((results) => (
+      results.map((result) => ({
         status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
-    });
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }))
+    ));
 }
