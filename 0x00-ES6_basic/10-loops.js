893657ca8791f7a92b250a3fb67c633cf3602a7e
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-03 20:27:01 +0300

    for .. of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
+  const nwArray = [];
+  for (const value of array) {
+    nwArray.push(appendString + value);
   }
 
-  return array;
+  return nwArray;
 }
