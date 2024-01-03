823ec8a0cb48ed58a0909f8cf60ba55ce3d9fa7c
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-03 11:14:44 +0300

    catching response

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,12 @@
 function handleResponseFromAPI(promise) {
-  promise
-    .then((result) => {
-      console.log("Got a response from the API");
-      return {status: 200, body: "success"};
-    })
-    .catch((error) => {
-      return new Error();
-    });
+  return promise
+    .then(() => (
+      { status: 200, body: "success" }
+    ))
+    .catch(() =>
+       new Error()
+       )
+     .finally(() => 
+       console.log("Got a response from the API"));
 }
 export default handleResponseFromAPI;
