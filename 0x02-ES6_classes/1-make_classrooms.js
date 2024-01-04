6e68a6270dd4995f210c6ddfd6896665234216da
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-04 17:00:03 +0300

    Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,16 @@
 import ClassRoom from './0-classroom.js';
 
+/**
+ *function initializeRooms() {
+ * const room1 = new ClassRoom(19);
+ *const room2 = new ClassRoom(20);
+ *const room3 = new ClassRoom(34);
+ *
+ *return [room1, room2, room3];
+ * }
+ */
+
 function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-
-  return [room1, room2, room3];
+  return [19, 20, 34].map((cat) => new ClassRoom(cat));
 }
-
 export default initializeRooms;
