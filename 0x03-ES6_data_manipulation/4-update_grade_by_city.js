5e1c052e92ee061fe3e985fe6ebcbe373e85a827
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-09 14:14:07 +0300

    arrow functions body must have {}

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
-      .map((student) => {
+      .map((student) => ({
       const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
       return {
         ...student,
         grade: gradeObject ? gradeObject.grade : 'N/A',
       };
-    });
+    }));
 }
 export default updateStudentGradeByCity;
