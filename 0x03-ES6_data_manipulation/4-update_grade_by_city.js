ac9c9644bf334b10d13be032b1f39073ec9ee353
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-01-10 10:55:59 +0300

    indentation correction

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,11 +2,11 @@ function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-    const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
-    return {
-      ...student,
-      grade: gradeObject ? gradeObject.grade : 'N/A',
-    };
-  });
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);    
+      return {
+        ...student,
+        grade: gradeObject ? gradeObject.grade : 'N/A',
+      };
+    });
 }
 export default updateStudentGradeByCity;
