dcb10dd629ce15c59322830a802418aa1aaff957
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-02-27 06:55:01 +0300

    created js interface

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// This script prints to STDOUT
+
+function displayMessage (message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// this script reads input from SDTIN
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (input) => {
+  if (input.trim() !== '') {
+    console.log(`Your name is: ${input}`);
+  }
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+});
