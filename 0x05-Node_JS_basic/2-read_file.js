c1a2203d123232069f152bb45d6f966a7024376d
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-03-02 04:38:02 +0300

    correct use of ++ in the script

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 // counting students
 
-function countStudents(path) {
+function countStudents (path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
     const rows = data.split('\n').filter((line) => line.trim() !== '');
