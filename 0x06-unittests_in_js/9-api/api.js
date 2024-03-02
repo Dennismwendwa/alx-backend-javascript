5d8d0f64c1e0dfa4c7996a64bfa981e1f4d3d37a
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-03-02 05:52:19 +0300

    updated files

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,14 +1,16 @@
 const express = require('express');
+
 const app = express();
 const PORT = 7865;
 app.get('/', (_, res) => {
-  res.send('Welcome');
+  res.send('Welcome to the payment system');
 });
 app.get('/cart/:id(\\d+)', (req, res) => {
   const id = req.params.id;
-  res.send(`Payment methods ${id}`);
+  res.send(`Payment methods for cart ${id}`);
 });
 app.listen(PORT, () => {
-  console.log(`API available port ${PORT}`);
+  console.log(`API available on localhost port ${PORT}`);
 });
+
 module.exports = app;
