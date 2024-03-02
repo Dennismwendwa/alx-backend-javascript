c9e1fcfac97a02b31e7b94a186bbeda253048893
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-03-02 04:27:01 +0300

    reading stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
 // this script reads input from SDTIN
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rl.on('line', (input) => {
-  if (input.trim() !== '') {
-    process.stdout.write(`Your name is: ${input}\n`);
+process.stdin.on('readable', () => {
+  const message = process.stdin.read();
+  if (message) {
+    process.stdout.write(`Your name is: ${message}`);
   }
 });
-
-rl.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
